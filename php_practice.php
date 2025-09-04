482e87f4f01c746250f33997436f19bcdda301db
Henjay-malakas <henjaycadiang42@gmail.com>
2025-09-04 18:52:03 +0800

    Add files via upload

---
+++ php_practice.php
@@ -0,0 +1,128 @@
+<!--Hello!
+<h1>Hello!</h1>
+<style>
+    h1{
+        color:yellow;
+    }
+</style>
+<script>
+    alert("Hello")
+</script> -->
+<?php 
+//echo "<b>hellow world</b>";
+?>
+
+<!--<h1>hellow</h1>-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello thid is me!</h1>
+    <?php
+//  Echo "Hello";
+   
+  $color = "Red";
+  $color = "blue";
+  //echo $color;
+  //echo $color;
+
+ /*$_ = 5;
+  echo $_;
+  var_dump($color);
+   var_dump($_);
+    var_dump(3.1);
+     var_dump(false);
+      var_dump([1,2]);
+
+      $x = 5;
+
+      function test(){
+        //$x = 10;
+        static $x = 10;
+        echo $x;
+        $x++;
+        echo "<br>";
+      }
+      test();
+      test();
+      test();
+      test();
+      test();
+      test();*/
+
+     // echo $x;
+
+    //  echo("Hello"),("Hello"),("Hello");
+    //  echo"World","World";
+    //  print("Hello");
+    //  print"World";
+
+    // $x = print("Hello");
+    // echo $x;
+
+
+    // $a = "miruem";
+    // $b = 1;
+    // $c = 1.2;
+    // $d = true;
+    // $e = [1,2,3];
+    // $f = new stdClass();
+    // $g = null;
+    // $h = fopen("php_practice.php","r");
+
+    // var_dump($a);
+    // echo "<br>";
+    // var_dump($b);
+    // echo "<br>";
+    // var_dump($c);
+    // echo "<br>";
+    // var_dump($d);
+    // echo "<br>";
+    // var_dump($e);
+    // echo "<br>";
+    // var_dump($f);
+    // echo "<br>";
+    // var_dump($g);
+    // echo "<br>";
+    // var_dump($h);
+    // echo "<br>";
+
+    // echo strlen($a);
+    
+
+    // $x = "juan cruz";
+    // echo str_replace("cruz ","Lance",$x);
+    // echo trim("Hello         ");
+
+    // var_dump (explode(" ", $x));
+
+    // $x = "Hello";
+    // $y = "RJ";
+
+    // echo $x . " " .$y, "<br>";
+    // echo "$x  $y","<br>";
+    // echo"john \"john\" paldo";
+
+    // echo is_int(1), "<br>"
+
+    // echo PHP_FLOAT_MAX;
+    // echo PHP_FLOAT_MIN;
+
+    // echo is_float(5.1);
+
+    echo is_numeric("5");
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
